75c7a14985fe78e4d9ab52115054dd2a1b6e3018
joemacaco <joe23y@gmail.com>
2017-06-25 03:36:50 +0800

    react-cdnjs-flux-container-components-example

---
+++ todo-cdn-flux/components/TodoHeaderContainer.js
@@ -0,0 +1,35 @@
+const {
+  TodoStore,
+  TodoHeader
+} = window.App;
+
+class TodoHeaderContainer extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      todos: TodoStore.getAll()
+    };
+  }
+
+  componentDidMount() {
+    this._removeChangeListener = TodoStore.addChangeListener(
+      () => this.setState({ todos: TodoStore.getAll() })
+    );
+  }
+
+  componentWillUnmount() {
+    this._removeChangeListener();
+  }
+
+  render() {
+    return (
+      <TodoHeader
+        title="我的待辦清單"
+        username="Joe"
+        todoCount={this.state.todos.filter((todo) => !todo.completed).length}
+      />
+    );
+  }
+}
+
+window.App.TodoHeaderContainer = TodoHeaderContainer;
