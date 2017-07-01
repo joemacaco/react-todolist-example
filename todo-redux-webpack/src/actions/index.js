cf54baf3258a87b9dd89246d3eea255be8aad902
joemacaco <joe23y@gmail.com>
2017-07-01 14:40:26 +0800

    react redux webpack example

---
+++ todo-redux-webpack/src/actions/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable import/newline-after-import */
+/* Exports all the actions from a single point.
+
+Allows to import actions like so:
+
+import {action1, action2} from '../actions/'
+*/
+/* Populated by react-webpack-redux:action */
+// const actions = {};
+// module.exports = actions;
+let nextTodoId = 0
+export const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+  }
+}
+
+export const setVisibilityFilter = (filter) => {
+  return {
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+  }
+}
+
+export const toggleTodo = (id) => {
+  return {
+    type: 'TOGGLE_TODO',
+    id
+  }
+}
